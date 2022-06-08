b41e4b179e356a8a6243efbd527d1764fdc66233
Namrata Barot <namrata.barot3@gmail.com>
2022-06-07 23:15:17 -0400

    Adding runtime permission

---
+++ app/src/main/java/com/lambton/lab1_namrata_c0853345_android/MapoverlaysActivity.java
@@ -0,0 +1,108 @@
+package com.lambton.lab1_namrata_c0853345_android;
+
+import android.Manifest;
+import android.graphics.Color;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.Dot;
+import com.google.android.gms.maps.model.Gap;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PatternItem;
+import com.google.android.gms.maps.model.Polygon;
+import com.google.android.gms.maps.model.PolygonOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
+
+public class MapoverlaysActivity extends AppCompatActivity {
+    MapView mapView;
+    GoogleMap googleMap;
+    EditText location_address;
+    Button add_location;
+    ArrayList<LatLng> cityCoordinates = new ArrayList<LatLng>();
+    private static final String MAP_VIEW_BUNDLE_KEY = "MapViewBundleKey";
+    //for poliline
+    private static final int PATTERN_GAP_LENGTH_PX = 20;
+    private static final PatternItem DOT = new Dot();
+    private static final PatternItem GAP = new Gap(PATTERN_GAP_LENGTH_PX);
+    // Create a stroke pattern of a gap followed by a dot.
+    private static final List<PatternItem> PATTERN_POLYLINE_DOTTED = Arrays.asList(GAP, DOT);
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mapoverlays_layout);
+        findId();
+        locationPermissionRequest();
+       // add_location.setOnClickListener(this);
+        Bundle mapbundle = null;
+        if (savedInstanceState != null)
+        {
+            mapbundle=savedInstanceState.getBundle(MAP_VIEW_BUNDLE_KEY);
+        }
+       // mapView.onCreate(mapbundle);
+       // mapView.getMapAsync(this);
+
+    }
+
+    private void findId() {
+        mapView=findViewById(R.id.mapView);
+        location_address=findViewById(R.id.location_address);
+        add_location=findViewById(R.id.add_location);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    private void locationPermissionRequest(){
+        ActivityResultLauncher<String[]> locationPermissionRequest =
+                registerForActivityResult(new ActivityResultContracts
+                                .RequestMultiplePermissions(), result -> {
+                            Boolean fineLocationGranted = result.getOrDefault(
+                                    Manifest.permission.ACCESS_FINE_LOCATION, false);
+                            Boolean coarseLocationGranted = result.getOrDefault(
+                                    Manifest.permission.ACCESS_COARSE_LOCATION,false);
+                            if (fineLocationGranted != null && fineLocationGranted) {
+                                // Precise location access granted.
+                                Toast.makeText(this," Precise location access granted",Toast.LENGTH_SHORT).show();
+                            } else if (coarseLocationGranted != null && coarseLocationGranted) {
+                                // Only approximate location access granted.
+                                Toast.makeText(this,"Only approximate location access granted",Toast.LENGTH_SHORT).show();
+                            } else {
+                                // No location access granted.
+                                Toast.makeText(this,"No location access granted",Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                );
+        locationPermissionRequest.launch(new String[] {
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+        });
+    }
+
+}
