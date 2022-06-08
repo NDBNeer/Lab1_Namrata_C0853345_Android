15c1cc8d8cdbc267afe354ffdcd3439a94048dbd
Namrata Barot <namrata.barot3@gmail.com>
2022-06-07 22:03:09 -0400

    Initial commit

---
+++ app/src/main/java/com/lambton/lab1_namrata_c0853345_android/MainActivity.java
@@ -0,0 +1,13 @@
+package com.lambton.lab1_namrata_c0853345_android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
